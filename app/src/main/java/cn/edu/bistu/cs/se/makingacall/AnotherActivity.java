a99b108971aa4b8a4fba29711419aa8d304f8c46
ZhangXinJie6 <211088842@qq.com>
2016-09-05 09:23:15 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/makingacall/AnotherActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.bistu.cs.se.makingacall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
